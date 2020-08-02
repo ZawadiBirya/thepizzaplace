3ba28d22f255e2e9ca58f45cfd444b8b31190af2
zawadi.birya <zawadibirya@gmail.com>
2020-08-02 23:49:35 +0300

    adding an background cover and form with appropriate input fields

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Land on the Homepage
+//Select Pizza size
+//Select Crust
+//Select Toppings
+//Deliver or collect
+//If it's collect get total
+//If it's deliver, get total + delivery cost
+alert("Welcome to the Pizza Place, please place your order!")
+
+function pizzaOrder(){
+
+var size = ["small" , "medium" , "large"];
+var crust = ["crispy" , "stuffed" , "gluten-free"];
+var toppings = ["beef" , "chicken" , "mushrooms"];
+var pizzaOrder = size + crust + toppings;
+
+ =  {
+getElementById("total").innerHTML = "Your order is + " ;
+}
+//Size
+//Crust
+//Toppings
+}
