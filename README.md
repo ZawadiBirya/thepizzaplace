d7bd5c4055e9df856ad8d408e51be166fbebff3d
zawadi.birya <zawadibirya@gmail.com>
2020-08-14 06:26:14 +0300

    adding bootstrap and jquery

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # thepizzaplace
-
-This is a web app where you can go to order a pizza , calculate your order and get it delivered!
-Technologies: Html, CSS, JS
-Live link: https://zawadibirya.github.io/thepizzaplace/index.html 
-
--- js/script.js
+++ js/script.js
@@ -1,71 +1,27 @@
-//Land on the Homepage
-//Select Pizza size
-//Select Crust
-//Select Toppings
-//Deliver or collect
-//If it's collect get total
-//If it's deliver, get total + delivery cost
-alert("Welcome to the Pizza Place, please place your order!")
+$(document).ready(function() {
+    $("form").submit(function(event) {
+      var sm = parseInt($("option#sm").val());
+      var md = parseInt($("option#md").val());
+      var lg = parseInt($("option#lg").val());
+      var crispy = parseInt($("option#crispy").val());
+      var stuffed = parseInt($("option#stuffed").val());
+      var glutenfree = parseInt($("option#glutenfree").val());
+      var mushrooms = parseInt($("option#mushrooms").val());
+      var beef = parseInt($("option#beef").val());
+      var chicken = parseInt($("option#chicken").val());
+      var deliver = parseInt($("input#deliver").val());
+      var collect = parseInt($("input#collect").val());
 
+      if (sm && crispy && mushrooms && deliver) {
+        alert (sm + crispy + mushrooms + deliver);
+       }else if (md && stuffed && beef && deliver) {
+        alert (md + stuffed + beef + deliver);
+      } else if (lg && glutenfree && chicken && deliver) {
+        alert (lg + glutenfree + chicken + deliver);
+      }
 
-//use JS libraries to store the data in my variables.
-let size =
-   {
-  small: 100 ksh,
-  medium : 150 ksh,
-  large: 200 ksh,
-}
-
-let toppings = {
-    chicken: 100,
-    beef: 150,
-    mushrooms: 200,
-}
-
-let crust =  {
-    crispy: 100,
-    stuffed: 150,
-    glutenfree: 200,
-  }
-
-let delivery = {
-
-}
-
-function pizzaOrder(){
-  if ((size.small) && (toppings.chicken) && (crust.crispy)){
-  getElementById("total").innerHTML = "Your order is" + size.small + toppings.chicken + crust.crispy";
-};
-
-else if ((size.small) && (toppings.beef) && (crust.stuffed)){
-getElementById("total").innerHTML = "Your order is" + size.small + toppings.beef + crust.crispy";
-};
-
-else if ((size.small) && (toppings.mushrooms) && (crust.glutenfree)){
-getElementById("total").innerHTML = "Your order is" + size.small + toppings.mushroom + crust.crispy";
-};
-
-if ((size.medium) && (toppings.chicken) && (crust.crispy)){
-getElementById("total").innerHTML = "Your order is" + size.medium + toppings.chicken + crust.crispy";
-};
-
-else if ((size.medium) && (toppings.beef) && (crust.stuffed)){
-getElementById("total").innerHTML = "Your order is" + size.medium + toppings.beef + crust.crispy";
-};
-
-else if ((size.medium) && (toppings.mushrooms) && (crust.glutenfree)){
-getElementById("total").innerHTML = "Your order is" + size.medium + toppings.mushroom + crust.crispy";
-};
-
-if ((size.large) && (toppings.chicken) && (crust.crispy)){
-getElementById("total").innerHTML = "Your order is" + size.large + toppings.chicken + crust.crispy";
-};
-
-else if ((size.large) && (toppings.beef) && (crust.stuffed)){
-getElementById("total").innerHTML = "Your order is" + size.large + toppings.beef + crust.crispy";
-};
-
-else if ((size.large) && (toppings.mushrooms) && (crust.glutenfree)){
-getElementById("total").innerHTML = "Your order is" + size.large + toppings.mushroom + crust.crispy";
-};
-function pizzaOrder();
+      $("p").show();
+      $('p').css('background-color', 'white');
+      event.preventDefault();
+    });
+});
